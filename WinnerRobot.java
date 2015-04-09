9528e0d05c5bff4437e100695eea5b2d4637e7ba
GuilhermeBernal <dev@lbguilherme.com>
2015-04-08 21:27:18 -0300

    Inicial

---
+++ WinnerRobot.java
@@ -0,0 +1,26 @@
+package ufba.winners;
+import robocode.*;
+
+public class WinnerRobot extends Robot {
+
+	public void run() {
+		while(true) {
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(1);
+	}
+
+	public void onHitByBullet(HitByBulletEvent e) {
+		back(10);
+	}
+	
+	public void onHitWall(HitWallEvent e) {
+		back(20);
+	}	
+}
